12dddd5dd681813c31408489618b04265e653bcd
ananyagaur-23 <ananyagaur.23@gmail.com>
2025-05-22 00:03:08 +0530

    Clean up old repo contents

--- 02/Hash3/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] nums = {100, 4, 200, 1, 3, 2};
-
-        int result = LongestConsecutiveSequence.longestConsecutive(nums);
-
-        System.out.println("Longest consecutive sequence length: " + result);
-    }
-}
